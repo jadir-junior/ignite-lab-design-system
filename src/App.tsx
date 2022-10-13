e973e1e8dba9edd920f96c4de9a48f31441ec91f
Jadir Junior <jadir.junior@gmail.com>
2022-10-13 09:22:59 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "./styles/global.css";
+
+function App() {
+  return <h1>Hellow World!</h1>;
+}
+
+export { App };
---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,17 @@
+import * as CheckboxRadix from "@radix-ui/react-checkbox";
+
+import { Check } from "phosphor-react";
+
+export interface CheckboxProps {}
+
+function Checkbox({}: CheckboxProps) {
+  return (
+    <CheckboxRadix.Root className="w-6 h-6 p-[2px] bg-gray-800 rounded">
+      <CheckboxRadix.Indicator asChild>
+        <Check weight="bold" className="h5 w-5 text-cyan-500" />
+      </CheckboxRadix.Indicator>
+    </CheckboxRadix.Root>
+  );
+}
+
+export { Checkbox };
